35507ecc8e56eff82f2b2d1b78e7b5bac6861cef
Adam Behoun <adam.behoun@seznam.cz>
2025-02-21 01:10:15 +0100

    refactor: move result printing from Program.cs to method

--- ScannerLibrary/Parser.cs
+++ ScannerLibrary/Parser.cs
@@ -32,5 +32,24 @@ namespace ScannerLibrary
                 Console.WriteLine(errs.ElementAt(i));
             }
         }
+
+
+        public void PrintResults(ArgumentParser parser, IpHandler ipHandler)
+        {
+            if (parser.ParsedOptions != null)
+            {
+                Console.WriteLine("Interface - " + parser.ParsedOptions.Interface);
+                Console.WriteLine("Wait - " + parser.ParsedOptions.Wait);
+                Console.WriteLine("Subnets - ");
+                foreach (var subnet in parser.ParsedOptions.Subnets)
+                {
+                    Console.WriteLine(subnet + " "); 
+                    int hosts = ipHandler.GetNumberOfHosts(subnet);
+                    Console.Write($" - {hosts} hosts");
+                    Console.WriteLine();
+                }
+            }
+        } 
+        
     }
 }
